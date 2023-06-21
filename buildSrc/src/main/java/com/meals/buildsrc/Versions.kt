8683bc78a1f4dd403729c2c71d51ad95a7b924dd
Murad Awwad <muradbaniawwad@gmail.com>
2023-06-22 00:06:02 +0300

    [ATOM-09] migrate to KTS WIP

---
+++ buildSrc/src/main/java/com/meals/buildsrc/Versions.kt
@@ -0,0 +1,20 @@
+package com.meals.buildsrc
+
+object Versions {
+
+    const val gradlePlugin = "8.0.2"
+    const val kotlin = "1.7.20"
+    const val appCompat = "1.6.1"
+    const val material = "1.2.0-alpha02"
+    const val constraintLayout = "1.1.3"
+    const val jUnit = "4.13.2"
+    const val coroutines = "1.5.2"
+    const val composeViewModel = "2.6.1"
+    const val coil = "2.2.2"
+    const val navigationCompose = "2.7.0-beta01"
+    const val koin = "3.1.2"
+    const val testExtJunit = "1.1.5"
+    const val espressoCore = "3.5.1"
+    const val composeBomTest = "2022.10.00"
+
+}
